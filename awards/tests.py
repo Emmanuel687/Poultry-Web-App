847d2af0294212d5792d02c995dc5ccaedae95bb
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-09 13:03:43 +0300

    Dev:Ran Test for profile

--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Profile,Project,Rating
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile = Profile(user='manu', bio='your king is good', profile_photo='cloudlink.cloud')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
