5360aa4e436b178fbb692761c90298e529c1f611
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-09 13:55:53 +0300

    Dev:Created a home.view

--- awards/urls.py
+++ awards/urls.py
@@ -1,6 +1,14 @@
-from django.urls import re_path
+from django.urls import re_path,path
 from . import views
+from django.contrib.auth import views as auth_views
+from .views import ListProfileView, ListProjectView
 
 urlpatterns=[
-    re_path('^$',views.welcome,name = 'welcome'),
+    # re_path('^$',views.welcome,name = 'welcome'),
+    path('',views.home,name = 'home'),
+    path('project/<project_id>/',views.project,name ='project'),
+    path('search/', views.search_results, name='search_results'),
+    path('profile/', views.profile, name='profile'),
+    path('update/', views.update, name='update'),
+    path('upload_project/', views.upload_project, name='upload_project'),
 ]
--- awards/views.py
+++ awards/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http.response import Http404
+from .models import Project,User,Profile
+
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+# def welcome(request):
+#     return render(request, 'welcome.html')
+
+def home(request):
+    projects = Project.objects.all()
+    users = User.objects.all()
+    return render(request, 'home.html', {"projects":projects, "users": users})
+
