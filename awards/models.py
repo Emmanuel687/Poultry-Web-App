ef468156579e41dff9dd86b3a72d15b0ca155673
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-09 12:49:32 +0300

    Dev:Created a profile model in model.py file

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    bio = models.TextField(null=True)
+    profile_photo = CloudinaryField('profile_photo', null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
