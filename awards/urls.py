1d79f2860f66f8c12ba2cf74b86d483e7ce874b7
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-10 15:00:26 +0300

    Dev:Created URLS to handle requests

--- awards/urls.py
+++ awards/urls.py
@@ -16,4 +16,6 @@ urlpatterns=[
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('api/profile/', ListProfileView.as_view(), name="profile-all"),
+    path('api/project/', ListProjectView.as_view(), name="project-all"),
 ]
