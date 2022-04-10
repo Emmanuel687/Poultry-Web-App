c39a938077a267283f19da824e4807be63b842c1
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-10 14:39:40 +0300

    Dev:Added registration,login,logout urls

--- awards/urls.py
+++ awards/urls.py
@@ -11,4 +11,9 @@ urlpatterns=[
     path('profile/', views.profile, name='profile'),
     path('update/', views.update, name='update'),
     path('upload_project/', views.upload_project, name='upload_project'),
+    path('project/<project_id>/',views.project,name ='project'),
+    path('rate/<project_id>/',views.rate,name ='rate'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
