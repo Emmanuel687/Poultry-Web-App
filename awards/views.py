4d4c71a2179016e7ad5f8498b4b4b0f83e6e5975
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-10 14:17:02 +0300

    Dev:Created a UserRegister Form

--- awards/views.py
+++ awards/views.py
@@ -1,14 +1,5 @@
-from django.shortcuts import render,redirect
-from django.http.response import Http404
-from .models import Project,User,Profile
-
+from django.shortcuts import render
 
 # Create your views here.
-# def welcome(request):
-#     return render(request, 'welcome.html')
-
-def home(request):
-    projects = Project.objects.all()
-    users = User.objects.all()
-    return render(request, 'home.html', {"projects":projects, "users": users})
-
+def welcome(request):
+    return render(request, 'welcome.html')
